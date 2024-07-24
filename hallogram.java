1e4300abd2971ede32305ea3822a0169ae9ea166
priyarajasekaran27 <159292444+priyarajasekaran27@users.noreply.github.com>
2024-07-24 09:26:39 +0530

    Add files via upload

---
+++ hallogram.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class HelloWorld
+{
+    public static void main(String[] args)
+    {
+        int n,i,j;
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+       
+        for(i=1;i<=n;i++){
+            for(j=1;j<=n;j++){
+                if(i==1 || j==1 || i==n || j==n){
+               
+               
+                System.out.print(" *");
+                }
+                else{
+                     System.out.print("  ");
+                }
+            }System.out.println();
+        }
+    }
+}
